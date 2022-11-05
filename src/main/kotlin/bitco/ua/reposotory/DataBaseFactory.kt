26a865eba29235894314e2240b5fa8283e9304db
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2022-11-05 19:16:12 -0400

    change db url

--- src/main/kotlin/bitco/ua/reposotory/DataBaseFactory.kt
+++ src/main/kotlin/bitco/ua/reposotory/DataBaseFactory.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DataBaseFactory {
 
@@ -25,10 +26,18 @@ object DataBaseFactory {
         val config = HikariConfig()
 
         config.driverClassName = System.getenv("JDBC_DRIVER")
-        config.jdbcUrl = System.getenv("DATABASE_URL")
+       // config.jdbcUrl = System.getenv("DATABASE_URL")
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation="TRANSACTION_REPEATABLE_READ"
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val username = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        config.jdbcUrl = "jdbc:postgresql://"+uri.host+":"+uri.path+"?sslmode=require"+"&user=$username&password=$password"
+
+
         config.validate()
 
         return HikariDataSource(config)
